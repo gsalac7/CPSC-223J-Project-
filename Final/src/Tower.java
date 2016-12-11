d9270dc7746bab1fb102ff47d30706f298e80a40
Gabe Salac <gsalac7@gmail.com>
2016-12-10 21:30:44 -0800

    Add files via upload

--- Final/src/Tower.java
+++ Final/src/Tower.java
@@ -17,12 +17,14 @@ public class Tower extends Applet implements ActionListener{
 	Label enter = new Label("Enter the number of disks(<= 7)");
 	TextField number = new TextField();
 	
-	Graphics g;
+
 	int diskWidth; //base width, is 100, subtracts by 10, up to 30.
 	int diskHeight; //height is constant
 	int xPos;
 	int yPos;
-		
+	int counter;	
+	int x2;
+	int y2;
 	static Stack [] height = new Stack[3];
 	
 	//static int [] height; //number of disks on a peg i starting peg 0
@@ -36,8 +38,6 @@ public class Tower extends Applet implements ActionListener{
 		setLayout(null);
 		resize(600, 250);
 		
-		g = getGraphics();
-		
 		height[0] = new Stack(0); //3rd peg
 		height[1] = new Stack(0); //first peg
 		height[2] = new Stack(0); //second peg
@@ -47,6 +47,7 @@ public class Tower extends Applet implements ActionListener{
 		diskWidth = 100;
 		diskHeight = 10;
 		numDisks = 0;
+		
 		enter.setBounds(10, 10, 180, 20);	add(enter);
 		number.setBounds(200, 10, 25, 20);	add(number);
 		start.setBounds(260, 10, 80, 20);	add(start);
@@ -79,15 +80,29 @@ public class Tower extends Applet implements ActionListener{
 		height[from].pop();
 		height[to].push();
 		
-		//to associate the peg numbers with disks, 
-		//we will use the xPos, and yPos. 70 = 1st peg, 
-		//300 = 2nd peg, 530 = 3rd peg
+		//TimeUnit.SECONDS.sleep(1);
 		
+		
+		/* So here we might just cheap it out and try to do every possibility 
+		 * like from peg 0 - peg 2 or peg 1 to peg 0 etc.
+		 * ORRR we can do this the hard way and calculate the xPos and yPos
+		 * We might just have to create the disk1 - disk7 each with its own widths, every disk 
+		 * has the same height
+		 * when we do that, we will then need to calculate the appropriate xPos for each..
+		 * but how do we calculate the yPos????? hmmmmm
+		 * yPos is calculated based on the height of the pegs...
+		 */
 		
 		if (from == 0) { //remove top disk, and add to the to peg
-			g.clearRect(10 + 2 * xPos, 175 - 2 * yPos, 130 - 2 * diskWidth, 10);
+			xPos = 10;
 		}
-		
+		else if (from == 1) {
+			xPos = 300;
+		}
+		else if(from == 2) {
+			xPos = 500;
+		}
+		repaint(); //redraw the frame
 		
 		//System.out.println(from + "->" + to);
 	}
@@ -116,22 +131,65 @@ public class Tower extends Applet implements ActionListener{
 	}
 	
 	//initialize the pegs; first thing called when applet starts
-	public void paint(Graphics ge) {
-		ge.setColor(Color.BLUE);
-		ge.fillRect(20, 190, 560, 10); //base
-		ge.fillRect(70, 60, 10, 150); //first peg
-		ge.fillRect(300, 60, 10, 150); //second peg
-		ge.fillRect(530, 60, 10, 150); //third peg
+	public void paint(Graphics g) {
+		g.setColor(Color.BLUE);
+		g.fillRect(20, 190, 560, 10); //base
+		g.fillRect(70, 60, 10, 150); //first peg
+		g.fillRect(300, 60, 10, 150); //second peg
+		g.fillRect(530, 60, 10, 150); //third peg
 
 		//create the painting based on the input, starting with the biggest disk.
-		ge.setColor(Color.RED);
-		for (int i = 0; i < numDisks; i++) { //numDisks is equal to the initial height.
+		g.setColor(Color.RED);
+		for (counter = 0; counter < numDisks; counter++) { //numDisks is equal to the initial height.
 			//xPos and yPos are to adjust the positions of every new disk
 			//disk width shrinks the disk's size
-			ge.fillRect(10 + i * xPos, 175 - i * yPos, 130 - i * diskWidth, 10);	
+			g.fillRect(10 + counter * xPos, 175 - counter * yPos, 130 - counter * diskWidth, 10);	
 		}
-	
 		
+		System.out.println(counter);
+		System.out.println(xPos);
+		if (xPos > 0 && xPos < 300) {
+			drawFirst(g);
+		}
+		else if (xPos > 300 && xPos < 500) {
+			drawSecond(g);
+		}
+		else if (xPos > 500 && xPos < 600) {
+			drawThird(g);
+		}
+	}
+	
+	public void drawFirst(Graphics g) { //draws the first peg
+		counter -= 1;
+		g.clearRect(10 + counter * xPos, 175 - counter * yPos, 130 - counter * diskWidth, 10);
+		
+		xPos = 240; //300 - 60
+		g.fillRect(xPos, 175, 130 - counter * diskWidth, 10);
+	}
+	public void drawSecond(Graphics g) {
+		counter -= 1;
+		g.clearRect(10 + counter * xPos, 175 - counter * yPos, 130 - counter * diskWidth, 10);
+	}
+	public void drawThird(Graphics g) {
+		counter -= 1;
+		g.clearRect(10 + counter * xPos, 175 - counter * yPos, 130 - counter * diskWidth, 10);
+	}
+}
+
+
+class Disk {
+	int width, height, xPos, yPos;
+	
+	Disk() {
+		width = 100;
+		height = 10;
+	}
+	
+	Disk(int xPos, int yPos, int width, int height) {
+		this.xPos = xPos;
+		this.yPos = yPos;
+		this.width = width;
+		this.height = height;
 	}
 }
 
